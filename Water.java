f6b9b89a870f0ee6df5699f9827400641e06ed84
muskankousar147 <muskankousar@gmail.com>
2024-06-27 14:04:14 +0530

    task

---
+++ Water.java
@@ -0,0 +1,22 @@
+public class Water{
+public static void main(String[] args){
+System.out.println("Enter the Water Details ");
+//int,float,double,char,short,long,byte
+int Ph = 7;
+float Level = 482.230F;
+double Range = 6; 
+char Quality = 7;
+short Size = 200;
+long Quantity = 1400000000;
+byte WaterBodies= 10;
+
+
+System.out.println("The water ph is:"+Ph);
+System.out.println("The water level is:"+Level);
+System.out.println("The water range is:"+Range);
+System.out.println("The water qulaity is:"+Quality);
+System.out.println("The water size is:"+Size);
+System.out.println("The water quantity is:"+Quantity);
+System.out.println("The water waterbodies is:"+WaterBodies);
+}
+}  
