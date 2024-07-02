deea83f426edf9112727cc6d0723aab9122d7e61
muskankousar147 <muskankousar@gmail.com>
2024-07-02 10:58:41 +0530

    today's task

---
+++ AeroplaneDetailsRunner.java
@@ -0,0 +1,12 @@
+public class AeroplaneDetailsRunner{
+public static void main(String args[]){
+System.out.println("invoke Aeroplane Details");
+System.out.println("aeroplane Details");
+AeroplaneDetails.AeroplaneSystem();
+AeroplaneDetails.AeroplaneName();
+AeroplaneDetails.AeroplaneType();
+AeroplaneDetails.AeroplaneColour();
+AeroplaneDetails.AerportLocation();
+
+}
+}
