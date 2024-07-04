b5e754845ca27db3b8ab0904a6bc78d33bc1f6b1
muskankousar147 <muskankousar@gmail.com>
2024-07-04 10:23:27 +0530

    todays task

--- Water.java
+++ Water.java
@@ -1,22 +1,33 @@
 public class Water{
-public static void main(String[] args){
-System.out.println("Enter the Water Details ");
-//int,float,double,char,short,long,byte
-int Ph = 7;
-float Level = 482.230F;
-double Range = 6; 
-char Quality = 7;
-short Size = 200;
-long Quantity = 1400000000;
-byte WaterBodies= 10;
-
-
-System.out.println("The water ph is:"+Ph);
-System.out.println("The water level is:"+Level);
-System.out.println("The water range is:"+Range);
-System.out.println("The water qulaity is:"+Quality);
-System.out.println("The water size is:"+Size);
-System.out.println("The water quantity is:"+Quantity);
-System.out.println("The water waterbodies is:"+WaterBodies);
+public static void DrinksDetails(String args[]){
+boolean WaterName == true;
+System.out.println("Water details are");
 }
-}  
+public static void WaterType(String SoftWater){
+System.out.println("water drink type is");
+boolean SoftWater == true;
+if(SoftWater == true){
+System.out.println("drink type is:"+SoftWater);
+}
+else{
+System.out.println("drink type is not correct:"+SoftWater);
+}
+}
+public static void WaterLitter(int litter){
+boolean WaterLitter == true;
+if(DrinkLitter == true){
+System.out.println("number of litters:"+WaterLitter);
+}
+else{
+System.out.println("incorrect litters:"+WaterLitter);
+}
+}
+public static void BrandName(String Name){
+boolean BrandName == true;
+if(BrandName == true){
+System.out.println("name of the drink brand is:"+BrandName);
+}else{
+System.out.println("incorrect brand name:"+BrandName);
+}
+}
+}
