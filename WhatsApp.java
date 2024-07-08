d669c6a5c635582bf501e408c439ba513411ae29
muskankousar147 <muskankousar@gmail.com>
2024-07-08 20:39:58 +0530

    message

---
+++ WhatsApp.java
@@ -0,0 +1,30 @@
+public class WhatsApp{
+	
+public static void WhatsAppDetails(){
+boolean SendPhoto = true;
+if(SendPhoto == true){
+System.out.println(" send photo details is");
+}
+}    
+                  
+public static String App(){
+boolean AppDetails = true;
+if(AppDetails == true){
+System.out.println("WhatsApp details is");
+}
+}
+public static void Storage(String[] Mb){
+boolean Mb = true;
+if(Mb == true){
+System.out.println("total number of mb needed to store the whatsapp is");
+}
+}
+public static String Video(String[] videos){
+boolean Videos = true;
+if(Videos == true){
+System.out.println("Videos details is:");
+return "Videos";
+
+}
+}
+}
